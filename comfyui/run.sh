b8177b7d106328f8fd9cbe3b44cce9b1ae65ebfa
wwhvw <wangweihong>
2026-01-03 00:25:50 +0800

    add comfyui prompt

---
+++ comfyui/run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+mkdir -p \
+  storage \
+  storage-models/models \
+  storage-models/hf-hub \
+  storage-models/torch-hub \
+  storage-user/input \
+  storage-user/output \
+  storage-user/workflows
+
+docker run -it --rm \
+  --name comfyui \
+  --runtime nvidia \
+  --gpus all \
+  -p 8188:8188 \
+  -e HF_ENDPOINT="https://hf-mirrors.com" \
+  -v "$(pwd)"/storage:/root \
+  -v "$(pwd)"/storage-models/models:/root/ComfyUI/models \
+  -v "$(pwd)"/storage-models/hf-hub:/root/.cache/huggingface/hub \
+  -v "$(pwd)"/storage-models/torch-hub:/root/.cache/torch/hub \
+  -v "$(pwd)"/storage-user/input:/root/ComfyUI/input \
+  -v "$(pwd)"/storage-user/output:/root/ComfyUI/output \
+  -v "$(pwd)"/storage-user/workflows:/root/ComfyUI/user/default/workflows \
+  -e CLI_ARGS="--disable-xformers" \
+  yanwk/comfyui-boot:cu128-slim
